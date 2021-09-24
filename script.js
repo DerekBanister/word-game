2c736b86ac6e88d5114f0a99e611a739ce294f3b
Derek Banister <ptownderek@yahoo.com>
2021-09-24 14:13:51 -0700

    pseudocode

---
+++ script.js
@@ -0,0 +1,27 @@
+//We want to declare global variables that are the words used in our game. Possibly an array of possible words that is selected from each time a function is called upon
+//var Names1 = [
+ //   duck
+  //  cow
+  //  Bird
+  //  muskrat
+//]
+
+
+
+//Create a function that has an eventlistener that begins the game and starts a timer when the button is clicked.
+
+
+
+//Create a seperate function that handles the timer
+
+
+
+//We need also create a function that tracks the users key inputs and relates them to the name that has been chosen in our variables names1
+
+
+
+//set localstorage to track whether the user wins the game or loses the game
+
+
+
+//Make a scoreboard that displays the localstorage data
